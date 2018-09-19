0617bc3f192f0e0dc886a3bc327ef1a53c2692df
xieyiming <329715712@qq.com>
2018-09-19 23:07:16 +0800

    initial

---
+++ geliman/js/basic.js
@@ -0,0 +1,17 @@
+
+
+$(function () {
+
+    //加载公用部分，写程序时要删掉
+    $(".index-header").load("index-header.html");
+    $("header").load("header.html");
+    $("footer").load("footer.html");
+
+
+});
+
+
+
+
+
+
