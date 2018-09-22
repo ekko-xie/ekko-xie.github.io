1da1563cc390b40ef142136b4753482131e4771a
xieyiming <329715712@qq.com>
2018-09-22 10:25:42 +0800

    index

---
+++ feinimochong/js/basic.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+
+
+
+
+
+$(function () {
+
+    //加载公用部分，写程序时要删掉
+    $(".index-header").load("index-header.html");
+    $("header").load("header.html");
+    $("footer").load("footer.html");
+
+
+});
+
+
+
+
+
+
