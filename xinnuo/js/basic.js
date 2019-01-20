fc7db3b47db3f879917189e11ea648b3755475db
xieyiming <329715712@qq.com>
2019-01-20 10:46:58 +0800

    xinnuo get

--- xinnuo/js/basic.js
+++ xinnuo/js/basic.js
@@ -9,8 +9,8 @@
 $(function () {
 
     //加载公用部分，写程序时要删掉
-    // $("header").load("header.html");
-    // $("footer").load("footer.html");
+    $("header").load("header.html");
+    $("footer").load("footer.html");
 
 
 });
