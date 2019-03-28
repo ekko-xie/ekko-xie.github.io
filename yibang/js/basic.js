0953c03bac7ad33c13864d5181d447f3a52e8b51
xieyiming <329715712@qq.com>
2019-03-28 17:36:04 +0800

    ad

--- yibang/js/basic.js
+++ yibang/js/basic.js
@@ -3,8 +3,8 @@
 $(function () {
 
     //加载公用部分，写程序时要删掉
-    // $("header").load("header.html");
-    // $("footer").load("footer.html");
+    $("header").load("header.html");
+    $("footer").load("footer.html");
 
 
 });
